49dc440cbf9d3f4fa30ca8bb38c125999ba251c2
Cristian Llanos <cristian.llanos@gmail.com>
2023-07-26 11:27:00 -0400

    first commit

---
+++ src/main/java/cl/bci/evaluacion/EvaluacionApplication.java
@@ -0,0 +1,16 @@
+package cl.bci.evaluacion;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class EvaluacionApplication {
+
+	public static void main(String[] args) {
+		log.info("=== iniciando EvaluacionApplication ===");
+		SpringApplication.run(EvaluacionApplication.class, args);
+	}
+
+}
